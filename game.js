f6b5c29d656fcb3320fee167423da2779b6f862d
Victor Hom <vh62@cornell.edu>
2015-10-13 12:30:19 -0400

    cleaned up the code

--- game.js
+++ game.js
@@ -1,44 +1,45 @@
 (function(){
 	window.onload = function () {
-
 		//if gamestate is false, the game cannot go on until you reset
-		//change the reset button so that user knows to click and reset gamestate
-		var gamestate = true;
-		var chances = 5;
-		var lr = 1;
-		var hr = 100;
+		var gamestate = true, chances = 5, lr = 1, hr = 100;
 		var adjustedhr = hr - lr + 1;
 		var numr = Math.floor(Math.random()*adjustedhr) + lr;
 		console.log(numr);
+
 		// given a wrong response, do some quick check to tell if cold or hot.
 		//if 20 off in either direction of the generated number, it is bad
 		function respondWithCloseness(g) {
-			if (Math.abs(numr-g) > 20 ){
-				return "you're pretty cold";
+			if (Math.abs(numr - g) > 20 ) {
+				return "you're cold and I don't care.";
 			} else {
-				return "you're hot, but what's the point?"
+				return "you're close and I am sad."
 			}
 			
 		}
-		//issue if you submit before talking.js completes the text still continues to writes
+		//helper function to update what text is in this element
+		function updateMessage(s, sty) {
+			document.getElementById("myTypingText").innerHTML = s;
+			document.getElementById("myTypingText").style.color = sty;
+		}
+
+		//issue if you submit before talking.js completes the text still continues to writes. unsure how to change
 		function doesNumberToGuessExist() {
 			if (gamestate) {
 			 	var guess  = parseInt(document.getElementById("number").value, 10);
+			 	//thought this would help the talking.js problem
 			 	document.getElementById("myTypingText").innerHTML = "";
 				if (guess >= 1 && guess <= 100 ){
 			 		if (guess === numr) {
-			 			document.getElementById("myTypingText").innerHTML = "big whoop that you guessed it!";
-			 			document.getElementById("myTypingText").style.color = "#F9D510";
+			 			gamestate == false;
+			 			updateMessage("big whoop that you guessed it!","#F9D510");
 			 			document.getElementsByClassName("reset")[0].style.color = "#7f0000";
 					}else{
-						document.getElementById("myTypingText").style.color = "#7f0000";
-						document.getElementById("myTypingText").innerHTML = respondWithCloseness(guess);
+						updateMessage(respondWithCloseness(guess),"#7f0000");
 						var counter = document.getElementById("chances");
 						chances = chances - 1;
 						counter.innerHTML = chances +"X";
 						counter.style.color = "#7f0000";
 						if (chances === 0) {
-							//game is on lockdown until player reset
 							gamestate = false;
 							document.getElementsByClassName("reset")[0].style.color = "#7f0000";
 						}
@@ -63,8 +64,6 @@
 			} else {
 				document.getElementById("myTypingText").innerHTML = "just reset since your chances are up";
 			}
-			//do some math on the number and give a hint 
-			//it's even and less than your number
 		}
 
 		//event to handle the clicks
